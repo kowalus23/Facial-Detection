b75d22e824b52793fcf391dce57306d9a5fc1f46
kowalus23 <kowalus23@gmail.com>
2019-06-12 14:06:10 +0200

    added register componen + routing across every big component so far

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
 import Signin from "./components/Signin/Signin";
+import Register from "./components/Register/Register";
 
 
 const app = new Clarifai.App({
@@ -20,7 +21,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: false
     }
   }
 
@@ -56,24 +58,32 @@ class App extends Component {
   };
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route})
   };
 
   render() {
+    const { isSignedIn, box, route, imageUrl } = this.state;
     return (
       <div>
-        <Navigation onRouteChange={this.onRouteChange}/>
-        {this.state.route === 'signin'
-          ? <Signin onRouteChange={this.onRouteChange}/>
-          : <div>
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+        {route === 'home'
+          ? <div>
             <Logo/>
-            < Rank />
+            < Rank/>
             < ImageLinkForm
               onInputChange={this.onInputChange}
               onButtonSubmit={this.onButtonSubmit}
             />
-            <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
+            <FaceRecognition box={box} imageUrl={imageUrl}/>
           </div>
+          : (route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange}/>
+            : <Register/>)
         }
       </div>
     )
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
-const Signin = ({onRouteChange}) => {
+const Register = ({onRouteChange}) => {
   return (
     <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-75-m w-50-l mw6 shadow-3 center">
       <main className="pa4 black-80">
         <form className="measure">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-            <legend className="f1 fw6 ph0 mh0">Sign In</legend>
-            <div className="mt3">
-              <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"
+            <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="text" name="name"
                      id="email-address"/>
+            </div>
+            <div className="mt3">
+              <label className="db fw6 lh-copy f6" htmlFor="name">Email</label>
+              <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"
+                     id="name"/>
             </div>
             <div className="mv3">
               <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
@@ -22,10 +27,7 @@ const Signin = ({onRouteChange}) => {
               onClick={() => onRouteChange('home')}
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
-              value="Sign in"/>
-          </div>
-          <div className="lh-copy mt3">
-            <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+              value="Submit"/>
           </div>
         </form>
       </main>
@@ -33,4 +35,4 @@ const Signin = ({onRouteChange}) => {
   )
 };
 
-export default Signin;
+export default Register;
